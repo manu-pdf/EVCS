52103b742e99ef4826cd4522975442ba0be41f80
manu-pdf <62282436+manu-pdf@users.noreply.github.com>
2021-12-15 18:42:17 +0100

    Update steam_test.py

--- steam_test.py
+++ steam_test.py
@@ -2,7 +2,7 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 
-DaF = pd.read_csv(r"C:\Users\trach\Desktop\drinks.csv")
+DaF = pd.read_csv(r"https://raw.githubusercontent.com/manu-pdf/EVCS/main/drinks.csv")
 checker = st.checkbox("Show me the DF")
 if checker:
     st.dataframe(DaF)
